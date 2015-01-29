d952e4dd3bbb07e6b57d0f03702e454a89688dac
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-01-29 15:33:41 -0500

    imake: use gcc's preprocessor for imake
    Closes #20.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- imake.rb
+++ imake.rb
@@ -1,12 +1,16 @@
-require "formula"
-
 class Imake < Formula
   homepage "http://xorg.freedesktop.org"
   url "http://xorg.freedesktop.org/releases/individual/util/imake-1.0.7.tar.bz2"
   sha1 "52e236776133f217d438622034b8603d201a6ec5"
 
   depends_on "pkg-config" => :build
+  depends_on "gcc"
   depends_on :x11
+
+  patch :p0 do
+    url "https://trac.macports.org/browser/trunk/dports/x11/imake/files/patch-imakemdep.h.diff?rev=127961&format=txt"
+    sha1 "88a78651635d41072febe735dc76e75d44cf0957"
+  end
 
   resource "xorg-cf-files" do
     url "http://xorg.freedesktop.org/releases/individual/util/xorg-cf-files-1.0.5.tar.bz2"
@@ -15,8 +19,17 @@ class Imake < Formula
 
   def install
     ENV.deparallelize
+
+    # imake runtime is broken when used with clang's cpp
+    cpp_program = Formula["gcc"].prefix/"bin/cpp-#{Formula["gcc"].version_suffix}"
+    inreplace "imakemdep.h", /::CPPCMD::/, cpp_program
+    inreplace "imake.man", /__cpp__/, cpp_program
+
+    # also use gcc's cpp during buildtime to pass ./configure checks
+    ENV["RAWCPP"] = cpp_program
+
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
 
     resource("xorg-cf-files").stage do
       # Fix for different X11 locations.
@@ -24,7 +37,7 @@ class Imake < Formula
                 "define TopXInclude	-I#{MacOS::X11.include}"
       system "./configure", "--with-config-dir=#{lib}/X11/config",
                             "--prefix=#{HOMEBREW_PREFIX}"
-      system "make install"
+      system "make", "install"
     end
   end
 end
