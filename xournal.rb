4473601ef6eb4ccf253e1b22b6840dda9c932999
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-01 02:05:31 +0100

    xournal: revision bumped to use latest poppler

--- xournal.rb
+++ xournal.rb
@@ -1,9 +1,9 @@
 class Xournal < Formula
-  desc "An application for notetaking, sketching, keeping a journal using a stylus"
+  desc "An application for notetaking and sketching using a stylus"
   homepage "http://xournal.sourceforge.net"
   url "https://downloads.sourceforge.net/xournal/xournal-0.4.8.tar.gz"
   sha256 "233887a38136452dcb4652c35d08366fc7355f57ed46753db83e3e0f3193ef30"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-x11"
@@ -44,7 +44,7 @@ index 9f9f1a2..6b18256 100644
 -  xournal_LDADD = ttsubset/libttsubset.a @PACKAGE_LIBS@ $(INTLLIBS) -lX11 -lz -lm
 +  xournal_LDADD = ttsubset/libttsubset.a @PACKAGE_LIBS@ $(INTLLIBS) -lz -lm
  endif
-
+ 
 diff --git a/src/xo-file.c b/src/xo-file.c
 index a1c19c9..cc38d14 100644
 --- a/src/xo-file.c
@@ -52,16 +52,16 @@ index a1c19c9..cc38d14 100644
 @@ -31,10 +31,6 @@
  #include <glib/gstdio.h>
  #include <poppler/glib/poppler.h>
-
+ 
 -#ifndef WIN32
 - #include <gdk/gdkx.h>
 - #include <X11/Xlib.h>
 -#endif
-
+ 
  #include "xournal.h"
  #include "xo-interface.h"
 @@ -1275,50 +1271,8 @@ GList *attempt_load_gv_bg(char *filename)
-
+ 
  struct Background *attempt_screenshot_bg(void)
  {
 -#ifndef WIN32
@@ -74,8 +74,8 @@ index a1c19c9..cc38d14 100644
 -  Window x_root, x_win;
 -
 -  x_root = gdk_x11_get_default_root_xwindow();
--
--  if (!XGrabButton(GDK_DISPLAY(), AnyButton, AnyModifier, x_root,
+-  
+-  if (!XGrabButton(GDK_DISPLAY(), AnyButton, AnyModifier, x_root, 
 -      False, ButtonReleaseMask, GrabModeAsync, GrabModeSync, None, None))
 -    return NULL;
 -
@@ -86,16 +86,16 @@ index a1c19c9..cc38d14 100644
 -  if (x_win == None) x_win = x_root;
 -
 -  window = gdk_window_foreign_new_for_display(gdk_display_get_default(), x_win);
--
+-    
 -  gdk_window_get_geometry(window, &x, &y, &w, &h, NULL);
 -  cmap = gdk_drawable_get_colormap(window);
 -  if (cmap == NULL) cmap = gdk_colormap_get_system();
--
+-  
 -  pix = gdk_pixbuf_get_from_drawable(NULL, window,
 -     cmap, 0, 0, 0, 0, w, h);
--
+-    
 -  if (pix == NULL) return NULL;
--
+-  
 -  bg = g_new(struct Background, 1);
 -  bg->type = BG_PIXMAP;
 -  bg->canvas_item = NULL;
@@ -109,19 +109,19 @@ index a1c19c9..cc38d14 100644
    return FALSE;
 -#endif
  }
-
+ 
  /************** pdf annotation ***************/
 diff --git a/src/xo-misc.c b/src/xo-misc.c
 index 2af7f43..f9003bd 100644
 --- a/src/xo-misc.c
 +++ b/src/xo-misc.c
 @@ -2288,9 +2288,7 @@ void hide_unimplemented(void)
-   }
-
+   }  
+   
    /* screenshot feature doesn't work yet in Win32 */
 -#ifdef WIN32
    gtk_widget_hide(GET_COMPONENT("journalScreenshot"));
 -#endif
- }
-
+ }  
+ 
  // toggle fullscreen mode
