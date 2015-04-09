0acc8174e05ba0a1f62ef42d2747657e62d61919
Alex Dunn <dunn.alex@gmail.com>
2015-04-09 12:06:02 -0700

    fox 1.6.50
    Closes #50.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- fox.rb
+++ fox.rb
@@ -1,14 +1,12 @@
-require "formula"
-
 class Fox < Formula
   homepage "http://www.fox-toolkit.org/"
-  url "ftp://ftp.fox-toolkit.org/pub/fox-1.6.49.tar.gz"
-  sha1 "056a55ba7b4404af61d4256eafdf8fd0503c6fea"
+  url "http://ftp.fox-toolkit.org/pub/fox-1.6.50.tar.gz"
+  sha256 "342c751d2a7edab136ad26cb4483329da3c16c7abf2f283f318d666ef12d0887"
 
   # Development and stable branches are incompatible
   devel do
-    url "ftp://ftp.fox-toolkit.org/pub/fox-1.7.49.tar.gz"
-    sha1 "a787a1300ddaf0330bb6ba656deda248d6e44f7a"
+    url "http://ftp.fox-toolkit.org/pub/fox-1.7.50.tar.gz"
+    sha256 "cdc432807f047b312ab54a778dc898e429dcfecf513fe0d350a7952f523a71df"
   end
 
   depends_on :x11
@@ -26,5 +24,13 @@ class Fox < Formula
     # Unset LDFLAGS, "-s" causes the linker to crash
     system "make", "install", "LDFLAGS="
     rm bin/"Adie.stx"
+  end
+
+  test do
+    system bin/"reswrap", "-t",
+           "-o", testpath/"text.txt",
+           test_fixtures("test.jpg")
+    assert_match "\\x00\\x85\\x80\\x0f\\xae\\x03\\xff\\xd9",
+                 (testpath/"text.txt").read
   end
 end
