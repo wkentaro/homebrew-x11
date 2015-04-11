061d43e8b27815fcbd470a4e6e84715d3020ae8e
Alex Dunn <dunn.alex@gmail.com>
2015-04-10 17:23:33 -0700

    feh 12.2.1
    Closes #54.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- feh.rb
+++ feh.rb
@@ -1,9 +1,7 @@
-require "formula"
-
 class Feh < Formula
   homepage "http://feh.finalrewind.org/"
-  url "http://feh.finalrewind.org/feh-2.12.tar.bz2"
-  sha1 "30eb2b778858b1f4ce97e44c8225758185b0c588"
+  url "http://feh.finalrewind.org/feh-2.12.1.tar.bz2"
+  sha256 "9026ece01d79560e1eff9715fa1765eef82e22c766da5994ee787984a6f466a1"
 
   depends_on :x11
   depends_on "imlib2"
@@ -14,5 +12,9 @@ class Feh < Formula
     args << "exif=1" if build.with? "libexif"
     system "make", "PREFIX=#{prefix}", *args
     system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    system bin/"feh", "-v"
   end
 end
