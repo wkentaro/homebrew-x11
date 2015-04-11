6eedd73b4ca81f7e65655f01634a1ccfb6f7e20e
Alex Dunn <dunn.alex@gmail.com>
2015-04-10 17:46:39 -0700

    cardpeek 0.8.4
    Closes #55.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- cardpeek.rb
+++ cardpeek.rb
@@ -1,19 +1,8 @@
-require "formula"
-
 class Cardpeek < Formula
   homepage "http://pannetrat.com/Cardpeek/"
-
-  stable do
-    url "http://downloads.pannetrat.com/get/302b8a00996e9f4180ad/cardpeek-0.8.3.tar.gz"
-    mirror "https://raw.githubusercontent.com/DomT4/LibreMirror/master/Cardpeek/cardpeek-0.8.3.tar.gz"
-    sha1 "8cc9c0652f0214ec06badb5b86974c66ca035a43"
-
-    # Patch required to fix GTK3.14 fatal build issues. Already merged into HEAD.
-    patch :p0 do
-      url "https://cardpeek.googlecode.com/issues/attachment?aid=500005000&name=patch-for-gtk-3.14.patch&token=ABZ6GAe27u5TeVC93yVqB58IQsyy6FjQQw%3A1412564556195"
-      sha1 "33b27af98546f605e5ab1c4a894c7db8fc2045f8"
-    end
-  end
+  url "http://downloads.pannetrat.com/install/cardpeek-0.8.4.tar.gz"
+  mirror "https://github.com/DomT4/LibreMirror/raw/d1dff2e5714953308864a606618a85cbde2375a2/Cardpeek/cardpeek-0.8.4.tar.gz"
+  sha256 "9c78dfdf84493c551b49447e4bb46c8d7b33f0785b93893222b70b6115013a85"
 
   bottle do
     cellar :any
@@ -24,22 +13,25 @@ class Cardpeek < Formula
 
   head "https://cardpeek.googlecode.com/svn/trunk/"
 
-  depends_on "pkg-config" => :build
-  depends_on "glib"
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on :x11
-  depends_on "openssl"
+  depends_on "pkg-config" => :build
+  depends_on "glib"
   depends_on "gtk+3"
   depends_on "lua"
+  depends_on "openssl"
+  depends_on :x11
 
   def install
-    # always run autoreconf, neeeded to generate configure for --HEAD,
+    # always run autoreconf, needed to generate configure for --HEAD,
     # and otherwise needed to reflect changes to configure.ac
     system "autoreconf", "-i"
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    system bin/"cardpeek", "-v"
   end
 end
