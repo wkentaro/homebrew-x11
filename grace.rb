065f714fdc00a7fd04cb2a4ea1df5703df8a57b2
Alex Dunn <dunn.alex@gmail.com>
2015-04-23 20:13:02 -0700

    grace 5.1.15
    Closes #62.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- grace.rb
+++ grace.rb
@@ -1,22 +1,28 @@
-require 'formula'
-
 class Grace < Formula
-  homepage 'http://plasma-gate.weizmann.ac.il/Grace/'
-  url 'ftp://plasma-gate.weizmann.ac.il/pub/grace/src/grace5/grace-5.1.23.tar.gz'
-  sha1 '0bd9cd6e76c97210658098f3533b5cf6c037d0bd'
+  homepage "http://plasma-gate.weizmann.ac.il/Grace/"
+  url "https://mirrors.kernel.org/debian/pool/main/g/grace/grace_5.1.25.orig.tar.gz"
+  sha256 "751ab9917ed0f6232073c193aba74046037e185d73b77bab0f5af3e3ff1da2ac"
 
   depends_on :x11
-  depends_on 'pdflib-lite'
-  depends_on 'jpeg'
-  depends_on 'fftw'
-  depends_on 'lesstif'
+  depends_on "pdflib-lite"
+  depends_on "jpeg"
+  depends_on "fftw"
+  depends_on "lesstif"
 
   def install
     ENV.O1 # https://github.com/Homebrew/homebrew/issues/27840#issuecomment-38536704
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}",
                           "--enable-grace-home=#{prefix}"
+    system "make", "install"
+    share.install "fonts", "examples"
+    man1.install Dir["doc/*.1"]
+    doc.install Dir["doc/*"]
+  end
 
-    system "make install"
+  test do
+    system bin/"gracebat", share/"examples/test.dat"
+    assert_equal "12/31/1999 23:59:59.999",
+                 shell_output("#{bin}/convcal -i iso -o us 1999-12-31T23:59:59.999").chomp
   end
 end
