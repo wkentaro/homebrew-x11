16d949b651e39cfa4809e92bc15f29a901084935
Alex Dunn <dunn.alex@gmail.com>
2015-04-28 17:57:26 -0700

    klavaro 3.01
    Closes #66.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- klavaro.rb
+++ klavaro.rb
@@ -1,20 +1,22 @@
-require 'formula'
-
 class Klavaro < Formula
-  homepage 'http://klavaro.sourceforge.net/'
-  url 'https://downloads.sourceforge.net/project/klavaro/klavaro-2.01.tar.bz2'
-  sha1 '42967960fb511abe60c536c1ff6794a0c35f38e5'
+  homepage "http://klavaro.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/klavaro/klavaro-3.01.tar.bz2"
+  mirror "https://mirrors.kernel.org/debian/pool/main/k/klavaro/klavaro_3.01.orig.tar.bz2"
+  sha256 "ed71d522c29113d5d6517a65cbf95dafbe85ca4bb978139b804b98f128015e85"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'intltool' => :build
-  depends_on 'gtk+'
-  depends_on 'gtkdatabox'
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "gtk+3"
+  depends_on "gtkdatabox"
   depends_on :x11
 
   def install
-    ENV.append 'LDFLAGS', '-lgmodule-2.0'
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"klavaro", "--help-gtk"
   end
 end
