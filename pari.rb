b18ffef3d1b95db8ea51adeb7b003c8a03ffafa4
Alex Dunn <dunn.alex@gmail.com>
2015-05-07 13:53:38 -0700

    pari 2.7.3
    Closes #67.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- pari.rb
+++ pari.rb
@@ -1,9 +1,8 @@
-require "formula"
-
 class Pari < Formula
   homepage "http://pari.math.u-bordeaux.fr/"
-  url "http://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.7.2.tar.gz"
-  sha256 "ac76c99549d549273087daa554c4dabaf9239881da422f69acb05fa7a0ff10ac"
+  url "http://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.7.3.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/p/pari/pari_2.7.3.orig.tar.gz"
+  sha256 "38032944bf4182cecbe438b888ec095ae7768fdbdd2adc1f25251f724f25650a"
 
   bottle do
     sha1 "279df09e4ed8ef338a908d22b376be54de33900c" => :mavericks
@@ -22,7 +21,12 @@ class Pari < Formula
                           "--with-gmp=#{gmp}",
                           "--with-readline=#{readline}"
     # make needs to be done in two steps
-    system "make all"
-    system "make install"
+    system "make", "all"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"math.tex").write "$k_{n+1} = n^2 + k_n^2 - k_{n-1}$"
+    system bin/"tex2mail", testpath/"math.tex"
   end
 end
