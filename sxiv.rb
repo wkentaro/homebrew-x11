bc416c4bc464d6334c1fc7b597e2988dc3f570b4
Alex Dunn <dunn.alex@gmail.com>
2015-05-12 17:08:31 -0700

    sxiv 1.3.1
    Closes #70.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- sxiv.rb
+++ sxiv.rb
@@ -1,15 +1,15 @@
-require "formula"
-
 class Sxiv < Formula
   homepage "https://github.com/muennich/sxiv"
-  url "https://github.com/muennich/sxiv/archive/v1.2.tar.gz"
-  sha1 "69cacabdd60316edbf3eee076d8057df7f7be0ed"
+  url "https://github.com/muennich/sxiv/archive/v1.3.1.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/s/sxiv/sxiv_1.3.1.orig.tar.gz"
+  sha256 "9a30a1b036e1c17212128554709da3f2d65d3beaef2e0a73097af5e35cf11d0e"
 
   head "https://github.com/muennich/sxiv.git"
 
   depends_on :x11
   depends_on "imlib2"
   depends_on "giflib"
+  depends_on "libexif"
 
   def install
     system "make", "config.h"
