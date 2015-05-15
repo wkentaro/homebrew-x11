71dde5451b177c8deec4092076bdb960483e7be0
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-15 19:05:13 +0100

    gnumeric: add 1.12.22 bottle.

--- gnumeric.rb
+++ gnumeric.rb
@@ -3,6 +3,13 @@ class Gnumeric < Formula
   url "http://ftp.gnome.org/pub/GNOME/sources/gnumeric/1.12/gnumeric-1.12.22.tar.xz"
   sha256 "3908cfd6520b599eefefe222aadeaa5126394b54d9a9a7f5e0f938eb674dcf47"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "29f856544019bc46cbea64a12a94787c664ad5edae716d6ce5914258c2ef2eab" => :yosemite
+    sha256 "d27645b4aaf3066bb593b31aaf00468adaff35244e8fbee1050671b41756e14b" => :mavericks
+    sha256 "d4e2a984bc16c17e6d41bcb1992d16c9fb097bcac40b5ed3a053cf4b7eb4d334" => :mountain_lion
+  end
+
   option "python-scripting", "Enable Python scripting."
 
   depends_on "pkg-config" => :build
