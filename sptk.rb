bcb6806c76015fdab2f602a11fd65dafd84ed8e8
Alex Dunn <dunn.alex@gmail.com>
2015-05-16 17:35:54 -0700

    sptk 3.8
    Closes #73.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- sptk.rb
+++ sptk.rb
@@ -1,13 +1,16 @@
-require "formula"
-
 class Sptk < Formula
   homepage "http://sp-tk.sourceforge.net/"
-  url "https://downloads.sourceforge.net/sp-tk/SPTK-3.7.tar.gz"
-  sha1 "319b8850f4ea4a294311fbb53f2421088c7e5171"
+  url "https://downloads.sourceforge.net/project/sp-tk/SPTK/SPTK-3.8/SPTK-3.8.tar.gz"
+  sha256 "028d6b3230bee73530f3d67d64eafa32cf23eaa987545975d260d0aaf6953f2b"
 
   depends_on :x11
 
+  option "with-examples", "Install example data and documentation"
+
   conflicts_with "libextractor", :because => "both install `extract`"
+  conflicts_with "num-utils", :because => "both install `average`"
+  conflicts_with "boost-bcp", :because => "both install `bcp`"
+  conflicts_with "rcs", :because => "both install `merge`"
 
   fails_with :gcc do
     cause "Segmentation fault during linking."
@@ -17,9 +20,24 @@ class Sptk < Formula
     cause "Segmentation fault during linking."
   end
 
+  resource "examples-data" do
+    url "https://downloads.sourceforge.net/project/sp-tk/SPTK/SPTK-3.8/SPTKexamples-data-3.8.tar.gz"
+    sha256 "3f37f279540e63ca55f0630d4aff8ba123203b0062b69fe379a8f1a951fddca9"
+  end
+
+  resource "examples-pdf" do
+    url "https://downloads.sourceforge.net/project/sp-tk/SPTK/SPTK-3.8/SPTKexamples-3.8.pdf"
+    sha256 "3bf21903a13b6b3d57b9684ad3800610c141e9333fcf9ee9df8543b234e567ce"
+  end
+
   def install
     system "./configure", "CC=#{ENV.cc}", "--prefix=#{prefix}"
     system "make", "install"
+
+    if build.with? "examples"
+      (doc/"examples").install resource("examples-data"),
+                               resource("examples-pdf")
+    end
   end
 
   test do
