45e6f3a317167c46e052a614f4ed0cdd0526cbd4
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-24 11:46:39 +0100

    grace: add 5.1.25 bottle.

--- grace.rb
+++ grace.rb
@@ -3,6 +3,13 @@ class Grace < Formula
   url "https://mirrors.kernel.org/debian/pool/main/g/grace/grace_5.1.25.orig.tar.gz"
   sha256 "751ab9917ed0f6232073c193aba74046037e185d73b77bab0f5af3e3ff1da2ac"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "c43911a967268be84fcc6c63c95d7c31dceaa86bae27e93717c486ecad2b5a56" => :yosemite
+    sha256 "31d319f10c0416849a1e7dc408cde9f9025cf594ee9f9e4d07eef80177d9fc48" => :mavericks
+    sha256 "b0cd52ba67d4e68939b5dcb55993d74faa934a935c5c50e638610d6371105bc8" => :mountain_lion
+  end
+
   depends_on :x11
   depends_on "pdflib-lite"
   depends_on "jpeg"
