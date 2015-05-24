cb384f677318094596681ea9057caf660c672373
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-24 12:53:03 +0100

    bochs: add 2.6.7 bottle.

--- bochs.rb
+++ bochs.rb
@@ -3,6 +3,13 @@ class Bochs < Formula
   url "https://downloads.sourceforge.net/project/bochs/bochs/2.6.7/bochs-2.6.7.tar.gz"
   sha256 "a4354eeb699662da869b038e225160ec726b883f1b4f425a06cf56cbffbb8282"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "fb8b148b05b1a8530fe3acacd1d48f73b8e6e3c0280dcd42c53e59be86528b4c" => :yosemite
+    sha256 "b34000337f75595e77ae5c3bf194ad4b49a24bc363187921218e2ac359ad58f9" => :mavericks
+    sha256 "d8eec0430e123c2d8e54b6212ba005bb384fa72c7a827f6be15a106426f22382" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on :x11
   depends_on "gtk+"
