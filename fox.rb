9f58260048afb37be263be9b4ffa53c210be7ff6
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-24 12:05:34 +0100

    fox: add 1.6.50 bottle.

--- fox.rb
+++ fox.rb
@@ -3,6 +3,14 @@ class Fox < Formula
   url "http://ftp.fox-toolkit.org/pub/fox-1.6.50.tar.gz"
   sha256 "342c751d2a7edab136ad26cb4483329da3c16c7abf2f283f318d666ef12d0887"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    cellar :any
+    sha256 "f7e8e7b4ecc4e9cb469b909427adea9ccf72a59c3f89ffd6ea6b006839c6c208" => :yosemite
+    sha256 "369b4d8ef5f2dc7e0afae6aa465140c1617d7fb48a14eafd3234974ac3e1ffe0" => :mavericks
+    sha256 "08c75fa15ea4146a0fed0e417060a9faf82374884c32d6b0eba226bddf0ece58" => :mountain_lion
+  end
+
   # Development and stable branches are incompatible
   devel do
     url "http://ftp.fox-toolkit.org/pub/fox-1.7.50.tar.gz"
