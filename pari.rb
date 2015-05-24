f4b99d018745e6050da936bdf0294764508acf6e
BrewTestBot <brew-test-bot@googlegroups.com>
2015-05-24 11:28:14 +0100

    pari: update 2.7.3 bottle.

--- pari.rb
+++ pari.rb
@@ -5,9 +5,10 @@ class Pari < Formula
   sha256 "38032944bf4182cecbe438b888ec095ae7768fdbdd2adc1f25251f724f25650a"
 
   bottle do
-    sha1 "279df09e4ed8ef338a908d22b376be54de33900c" => :mavericks
-    sha1 "e7fba0b065c31e7f9b23d198f24f76333d28a8ba" => :mountain_lion
-    sha1 "8dcd6a003063962867a9f29830acd6eeea58e90f" => :lion
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "117b68816bcec0e692ae9517639a327de286eb43afc9d517e84c59fbb46cd55a" => :yosemite
+    sha256 "4e1b7bdb62f7ace93e4b12cfe0a4eed16e185fedee35f62350969081d4d589dc" => :mavericks
+    sha256 "64fc709bb96c40dd1a17dbc9695a51f33fc9e8112e34180ce016364c471e17e6" => :mountain_lion
   end
 
   depends_on "gmp"
