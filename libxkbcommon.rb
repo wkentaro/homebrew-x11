4e6bab27ffe9c317c028ee6ff8dfbf6364a16392
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-09 15:15:29 +0100

    libxkbcommon: add 0.5.0 bottle.

--- libxkbcommon.rb
+++ libxkbcommon.rb
@@ -5,6 +5,13 @@ class Libxkbcommon < Formula
   sha256 "90bd7824742b9a6f52a6cf80e2cadd6f5349cf600a358d08260772615b89d19c"
   head "https://github.com/xkbcommon/libxkbcommon.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "c138b2459677414251a3a904d51a3151a90f0a32909641b7ca003241c70ceb1c" => :yosemite
+    sha256 "65335a728e42b65b02d50508c1dcfea2f1173ab675b319d7060dcab464d9aa11" => :mavericks
+    sha256 "b04ed8bb44caee806c2fdb8a7b364ca556d26495a28099ce918f5a9ac3d73885" => :mountain_lion
+  end
+
   depends_on :x11
   depends_on "bison" => :build
   depends_on "pkg-config" => :build
