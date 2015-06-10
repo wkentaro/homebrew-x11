2b8beb7267517542e84ffd36d887053eee410715
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-10 01:54:01 +0100

    i3: add 4.10.2 bottle.

--- i3.rb
+++ i3.rb
@@ -5,6 +5,13 @@ class I3 < Formula
   sha256 "0795a31b47f93b637da7f7e65197568117f46a35c745f4de4f56e7b2efcccfd8"
   head "https://github.com/i3/i3.git"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "5038535280aefed22583c703f3acb059143176186d42f58a8f1713b4d7a59807" => :yosemite
+    sha256 "ac30c8e39a54383384119e572734703000113e406312fd1a7f8ca56fa489b924" => :mavericks
+    sha256 "c80d02dad19e0b391f88b05d747404addae95507118f6f156aba5ea0cb6e9db6" => :mountain_lion
+  end
+
   depends_on "asciidoc" => :build
   depends_on "pkg-config" => :build
   depends_on "cairo"
