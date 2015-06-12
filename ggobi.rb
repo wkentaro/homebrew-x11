0d7fe64c5bba18ae2efdc5e6679dc77d2f779307
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-06-12 16:17:24 +0100

    ggobi went quartz-only
    version bump
    audit --strict compliance

--- ggobi.rb
+++ ggobi.rb
@@ -1,95 +1,89 @@
-require 'formula'
-
 class Ggobi < Formula
-  homepage 'http://www.ggobi.org'
-  url 'http://www.ggobi.org/downloads/ggobi-2.1.9.tar.bz2'
-  sha1 '0dd0fe0cf619c1404d024e019cf9c8d87fb4fe4b'
+  desc "A visualization program for exploring high-dimensional data"
+  homepage "http://www.ggobi.org"
+  url "http://www.ggobi.org/downloads/ggobi-2.1.11.tar.bz2"
+  sha256 "2c4ddc3ab71877ba184523e47b0637526e6f3701bd9afb6472e6dfc25646aed7"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gtk+'
-  depends_on 'glib'
-  depends_on 'atk'
-  depends_on 'cairo'
-  depends_on 'fontconfig'
-  depends_on 'pango'
-  depends_on 'gettext'
-  depends_on 'libtool' => :run
-  depends_on :x11
-
-  # Several files reference "line 0", which gcc accepts but clang doesn't
-  # See https://github.com/Homebrew/homebrew/pull/13423
-  patch :DATA
+  depends_on "pkg-config" => :build
+  depends_on "gtk+"
+  depends_on "gettext"
+  depends_on "libtool" => :run
 
   def install
     # Necessary for ggobi to build - based on patch from MacPorts
     # See: https://trac.macports.org/export/64669/trunk/dports/science/ggobi/files/patch-src-texture.diff
     # Reported at https://groups.google.com/d/msg/ggobi/0yiepEUgjiM/nXTVoMaAzj8J
-    inreplace 'src/texture.c', 'psort', 'p_sort'
+    inreplace "src/texture.c", "psort", "p_sort"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-all-plugins"
-    system "make install"
+    system "make", "install"
+  end
+
+  test do
+    # executable (GUI)
+    system "#{bin}/ggobi", "--version"
+    # API
+    (testpath/"test.c").write <<-EOS.undent
+      #include <GGobiAPI.h>
+
+      int main(int argc, char *argv[]) {
+        const char *string = GGobi_getVersionString();
+        return 0;
+      }
+    EOS
+    ENV.libxml2
+    atk = Formula["atk"]
+    cairo = Formula["cairo"]
+    fontconfig = Formula["fontconfig"]
+    freetype = Formula["freetype"]
+    gdk_pixbuf = Formula["gdk-pixbuf"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    gtkx = Formula["gtk+"]
+    libpng = Formula["libpng"]
+    pango = Formula["pango"]
+    pixman = Formula["pixman"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{atk.opt_include}/atk-1.0
+      -I#{cairo.opt_include}/cairo
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{gtkx.opt_include}/gtk-2.0
+      -I#{gtkx.opt_lib}/gtk-2.0/include
+      -I#{include}/ggobi
+      -I#{libpng.opt_include}/libpng16
+      -I#{pango.opt_include}/pango-1.0
+      -I#{pixman.opt_include}/pixman-1
+      -D_REENTRANT
+      -L#{atk.opt_lib}
+      -L#{cairo.opt_lib}
+      -L#{gdk_pixbuf.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{gtkx.opt_lib}
+      -L#{lib}
+      -L#{pango.opt_lib}
+      -latk-1.0
+      -lcairo
+      -lgdk-quartz-2.0
+      -lgdk_pixbuf-2.0
+      -lggobi
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lgtk-quartz-2.0
+      -lintl
+      -lpango-1.0
+      -lpangocairo-1.0
+      -lxml2
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
   end
 end
-
-__END__
-diff --git a/src/ggobi-data.c b/src/ggobi-data.c
-index ccb52dc..fbd756e 100644
---- a/src/ggobi-data.c
-+++ b/src/ggobi-data.c
-@@ -42,7 +42,7 @@ typedef GGobiDataClass SelfClass;
- /* here are local prototypes */
- static void ___object_set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec);
- static void ___object_get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec);
--#line 0 "data.gob"
-+#line 1 "data.gob"
- static void ggobi_data_class_init (GGobiDataClass * c) G_GNUC_UNUSED;
- #line 48 "ggobi-data.c"
- #line 191 "data.gob"
-diff --git a/src/ggobi-renderer-cairo.c b/src/ggobi-renderer-cairo.c
-index 0b51ab6..12a47cf 100644
---- a/src/ggobi-renderer-cairo.c
-+++ b/src/ggobi-renderer-cairo.c
-@@ -57,10 +57,10 @@ struct _GGobiRendererCairoPrivate {
- /* here are local prototypes */
- static void ___object_set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec);
- static void ___object_get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec);
--#line 0 "renderer-cairo.gob"
-+#line 1 "renderer-cairo.gob"
- static void ggobi_renderer_cairo_init (GGobiRendererCairo * o) G_GNUC_UNUSED;
- #line 63 "ggobi-renderer-cairo.c"
--#line 0 "renderer-cairo.gob"
-+#line 1 "renderer-cairo.gob"
- static void ggobi_renderer_cairo_class_init (GGobiRendererCairoClass * c) G_GNUC_UNUSED;
- #line 66 "ggobi-renderer-cairo.c"
- #line 36 "renderer-cairo.gob"
-diff --git a/src/ggobi-renderer-factory.c b/src/ggobi-renderer-factory.c
-index ee1c93e..5be74bf 100644
---- a/src/ggobi-renderer-factory.c
-+++ b/src/ggobi-renderer-factory.c
-@@ -44,10 +44,10 @@ typedef GGobiRendererFactory Self;
- typedef GGobiRendererFactoryClass SelfClass;
-
- /* here are local prototypes */
--#line 0 "renderer-factory.gob"
-+#line 1 "renderer-factory.gob"
- static void ggobi_renderer_factory_init (GGobiRendererFactory * o) G_GNUC_UNUSED;
- #line 50 "ggobi-renderer-factory.c"
--#line 0 "renderer-factory.gob"
-+#line 1 "renderer-factory.gob"
- static void ggobi_renderer_factory_class_init (GGobiRendererFactoryClass * c) G_GNUC_UNUSED;
- #line 53 "ggobi-renderer-factory.c"
- static GGobiRenderer * ___real_ggobi_renderer_factory_create (GGobiRendererFactory * self, GdkDrawable * parent);
-diff --git a/src/ggobi-renderer.c b/src/ggobi-renderer.c
-index 1231145..7174c13 100644
---- a/src/ggobi-renderer.c
-+++ b/src/ggobi-renderer.c
-@@ -51,7 +51,7 @@ struct _GGobiRendererPrivate {
- /* here are local prototypes */
- static void ___object_set_property (GObject *object, guint property_id, const GValue *value, GParamSpec *pspec);
- static void ___object_get_property (GObject *object, guint property_id, GValue *value, GParamSpec *pspec);
--#line 0 "renderer.gob"
-+#line 1 "renderer.gob"
- static void ggobi_renderer_init (GGobiRenderer * o) G_GNUC_UNUSED;
- #line 57 "ggobi-renderer.c"
- static GdkDrawable * ___real_ggobi_renderer_create_target (GGobiRenderer * self, GdkDrawable * parent);
