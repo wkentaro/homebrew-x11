941116e6cd51b67ac1ca0f5538e66840e26f73dc
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-06-12 18:27:41 +0100

    gtk-chtheme went quartz-only
    revision bump
    audit --strict compliance

--- gtk-chtheme.rb
+++ gtk-chtheme.rb
@@ -1,23 +1,25 @@
-require 'formula'
-
 class GtkChtheme < Formula
-  homepage 'http://plasmasturm.org/code/gtk-chtheme/'
-  url 'http://plasmasturm.org/code/gtk-chtheme/gtk-chtheme-0.3.1.tar.bz2'
-  sha1 'dbea31f4092877e786fe040fae1374238fada94a'
+  desc "GTK+ 2.0 theme changer GUI"
+  homepage "http://plasmasturm.org/code/gtk-chtheme/"
+  url "http://plasmasturm.org/code/gtk-chtheme/gtk-chtheme-0.3.1.tar.bz2"
+  sha256 "26f4b6dd60c220d20d612ca840b6beb18b59d139078be72c7b1efefc447df844"
+  revision 1
 
-  depends_on 'pkg-config' => :build
-  depends_on 'gdk-pixbuf'
-  depends_on 'gettext'
-  depends_on 'glib'
-  depends_on 'gtk+'
-  depends_on 'pango'
-  depends_on :x11
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gtk+"
 
   def install
     # Unfortunately chtheme relies on some deprecated functionality
     # we need to disable errors for it to compile properly
-    inreplace 'Makefile', '-DGTK_DISABLE_DEPRECATED', ''
+    inreplace "Makefile", "-DGTK_DISABLE_DEPRECATED", ""
 
     system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    # package contains just an executable and a man file
+    # executable accepts no options and just spawns a GUI
+    assert (bin/"gtk-chtheme").exist?
   end
 end
