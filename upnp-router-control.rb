51fda104434e2aea7f9db5f1534f641c559d5a58
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-06-13 17:24:01 +0100

    upnp-router-control went quartz-only
    revision bump
    audit --strict compliance

--- upnp-router-control.rb
+++ upnp-router-control.rb
@@ -1,26 +1,25 @@
-require 'formula'
-
 class UpnpRouterControl < Formula
-  homepage 'https://launchpad.net/upnp-router-control'
-  url 'https://launchpad.net/upnp-router-control/trunk/0.2/+download/upnp-router-control-0.2.tar.gz'
-  sha1 '4d6b22430f784260fccb2f70c27d0a428b9a753a'
+  desc "UPnP router monitor tool"
+  homepage "https://launchpad.net/upnp-router-control"
+  url "https://launchpad.net/upnp-router-control/trunk/0.2/+download/upnp-router-control-0.2.tar.gz"
+  sha256 "c8a86a54687379fd7b85dc9156a3858897ee1d7af75354e68358243f03d03820"
+  revision 1
 
   head do
-    url 'lp:upnp-router-control', :using => :bzr
+    url "lp:upnp-router-control", :using => :bzr
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
-  depends_on 'pkg-config' => :build
-  depends_on 'intltool' => :build
-  depends_on 'gettext'
-  depends_on 'gtk+'
-  depends_on 'gupnp'
-  depends_on 'gssdp'
-  depends_on 'curl' => :optional
-  depends_on :x11
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "gettext"
+  depends_on "gtk+"
+  depends_on "gupnp"
+  depends_on "gssdp"
+  depends_on "curl" => :optional
 
   # Per Debian, patch to compile against newer gupnp
   patch :DATA
@@ -28,16 +27,20 @@ class UpnpRouterControl < Formula
   def install
     system "./autogen.sh" if build.head?
 
-    # Recent gupnp pc files don't export symbols from gssdp
+    # Recent gupnp pc files don"t export symbols from gssdp
     # Bug Ref: https://bugs.launchpad.net/upnp-router-control/+bug/1100236
-    if not build.head?
-      ENV.append_to_cflags %x[pkg-config --cflags gssdp-1.0].chomp
-      ENV['LIBS'] = %x[pkg-config --libs gssdp-1.0].chomp
+    unless build.head?
+      ENV.append_to_cflags `pkg-config --cflags gssdp-1.0`.chomp
+      ENV["LIBS"] = `pkg-config --libs gssdp-1.0`.chomp
     end
 
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    system "#{bin}/upnp-router-control", "--version"
   end
 end
 
