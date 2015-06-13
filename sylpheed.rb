2f2db49b094e4b53531d3c783f0acf36edc1b80f
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-06-13 13:08:45 +0100

    sylpheed went quartz-only
    revision bump
    audit --strict compliance

--- sylpheed.rb
+++ sylpheed.rb
@@ -1,9 +1,9 @@
-require "formula"
-
 class Sylpheed < Formula
+  desc "A simple, lightweight email-client"
   homepage "http://sylpheed.sraoss.jp/en/"
   url "http://sylpheed.sraoss.jp/sylpheed/v3.4/sylpheed-3.4.2.tar.gz"
-  sha1 "693514a792f64ac1a138c7a6542d5ca6b7ac4fdc"
+  sha256 "a4c47b570a5b565d14ff9933cf2e03fcb895034c1f072f9cd2c4a9867a2f2263"
+  revision 1
 
   bottle do
     sha1 "768d366c0eb0b8de1ad50ce405b05c20c5a6b2dc" => :yosemite
@@ -12,10 +12,8 @@ class Sylpheed < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on :x11
   depends_on "gpgme"
   depends_on "gtk+"
-  depends_on "cairo"
   depends_on "openssl"
 
   def install
@@ -23,5 +21,9 @@ class Sylpheed < Formula
                           "--prefix=#{prefix}",
                           "--disable-updatecheck"
     system "make", "install"
+  end
+
+  test do
+    system "#{bin}/sylpheed", "--version"
   end
 end
