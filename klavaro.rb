61770493d009753a538600473fb3b5bbe5dc9fc3
Tom Schoonjans <Tom.Schoonjans@bam.de>
2015-06-13 07:28:59 +0100

    klavaro cleanup
    removed redundant dependency on gtkdatabox
    desc added

--- klavaro.rb
+++ klavaro.rb
@@ -1,4 +1,5 @@
 class Klavaro < Formula
+  desc "A free touch typing tutor program"
   homepage "http://klavaro.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/klavaro/klavaro-3.01.tar.bz2"
   mirror "https://mirrors.kernel.org/debian/pool/main/k/klavaro/klavaro_3.01.orig.tar.bz2"
@@ -14,8 +15,6 @@ class Klavaro < Formula
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
   depends_on "gtk+3"
-  depends_on "gtkdatabox"
-  depends_on :x11
 
   def install
     system "./configure", "--disable-dependency-tracking",
