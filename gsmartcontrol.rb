0b00f8413d6f8544fd5e9419f23d8480bec21411
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-18 06:21:57 +0100

    gsmartcontrol: add 0.8.7_1 bottle.

--- gsmartcontrol.rb
+++ gsmartcontrol.rb
@@ -5,6 +5,13 @@ class Gsmartcontrol < Formula
   sha256 "708fa803243abb852ed52050fc82cd3592a798c02743342441996e77f19ffec6"
   revision 1
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "655cb81fca643ef4e8f0a049f528a03418f1592c78d3a190e5225dffb4ba6860" => :yosemite
+    sha256 "a5bc7d15d727ac081d84dd230175600b575f7ac233bc9b01d1660a3aff18aaef" => :mavericks
+    sha256 "0b98ddcecf9e9c300f6fa94306fe332215bd74bfce2786111e468fbd7d9f34f6" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "smartmontools"
   depends_on "gtkmm"
