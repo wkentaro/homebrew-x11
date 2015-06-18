80c8d6db4af4d0a0d91155dbd4cd21bb0f14bbbe
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-18 06:21:57 +0100

    gtk-chtheme: add 0.3.1_1 bottle.

--- gtk-chtheme.rb
+++ gtk-chtheme.rb
@@ -5,6 +5,14 @@ class GtkChtheme < Formula
   sha256 "26f4b6dd60c220d20d612ca840b6beb18b59d139078be72c7b1efefc447df844"
   revision 1
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    cellar :any
+    sha256 "4e1559fa2ef3535ab4b8e90eab0956be94fde3a34250a96663957a457d3a9a7c" => :yosemite
+    sha256 "697dcc6f584f5b3b2855aaec6ffe2c50a026de54c17b135c7c84079376fa3d4d" => :mavericks
+    sha256 "50c9eb5b3b7dd20bab68ee748b13162e5466c4699f2c12bb10d15ee4fb9e052e" => :mountain_lion
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "gtk+"
