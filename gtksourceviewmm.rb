1f4205f54b796bbb2befe89836e245b957556364
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-18 06:21:57 +0100

    gtksourceviewmm: add 2.10.3_1 bottle.

--- gtksourceviewmm.rb
+++ gtksourceviewmm.rb
@@ -5,6 +5,13 @@ class Gtksourceviewmm < Formula
   sha256 "0000df1b582d7be2e412020c5d748f21c0e6e5074c6b2ca8529985e70479375b"
   revision 1
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "3d8566e86cece40dc4a6b04c2c98918d536b9e4cc7afa77a97b607488913e02c" => :yosemite
+    sha256 "f66ec2e03070338f44ec5af6c43376e81631e47d423f38dd2e5eb3c188b4cb36" => :mavericks
+    sha256 "255b390316e7ea0ac7d8ca391b2825d83762f0a17ec323d87895b1742dcd3503" => :mountain_lion
+  end
+
   depends_on "gtksourceview"
   depends_on "pkg-config" => :build
   depends_on "gtkmm"
