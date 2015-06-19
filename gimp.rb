a7a239b02c84d00521595ed6651a547524169599
BrewTestBot <brew-test-bot@googlegroups.com>
2015-06-19 05:16:35 +0100

    gimp: add 2.8.14 bottle.

--- gimp.rb
+++ gimp.rb
@@ -5,6 +5,13 @@ class Gimp < Formula
   sha256 "d82a958641c9c752d68e35f65840925c08e314cea90222ad845892a40e05b22d"
   head "https://github.com/GNOME/gimp.git", :branch => "gimp-2-8"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-x11"
+    sha256 "fd5b0d50ae84e060b3056fbd9a4062a65705bcd3682b89982dca7e9316e290be" => :yosemite
+    sha256 "0fe19bce7983265a835dfa899325f0adaed4e966fc5a7a6c6af9c53bf4d2a62d" => :mavericks
+    sha256 "46fe567462e8ff5f76bc143b3321384f5cf06f430d13b2aa4851db7541d41ae0" => :mountain_lion
+  end
+
   depends_on :x11 => :recommended
   depends_on "pkg-config" => :build
   depends_on "gettext" => :build
