eeb7ce21aedacd861bdb28f9fae5b21b5b62b83c
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-19 05:01:04 +0100

    gimp: fix strict audit
    Closes #88.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- gimp.rb
+++ gimp.rb
@@ -1,41 +1,55 @@
-require "formula"
-
 class Gimp < Formula
+  desc "GNU Image Manipulation Program"
   homepage "http://www.gimp.org"
   url "http://download.gimp.org/pub/gimp/v2.8/gimp-2.8.14.tar.bz2"
-  sha1 "380a8e2887e1a161056444921807e338c3d31653"
+  sha256 "d82a958641c9c752d68e35f65840925c08e314cea90222ad845892a40e05b22d"
   head "https://github.com/GNOME/gimp.git", :branch => "gimp-2-8"
 
+  depends_on :x11 => :recommended
   depends_on "pkg-config" => :build
   depends_on "gettext" => :build
   depends_on "intltool" => :build
-
-  depends_on :x11
-
   depends_on "gegl"
   depends_on "babl"
   depends_on "fontconfig"
-
   depends_on "pango"
   depends_on "gtk+"
+  depends_on "gtk-mac-integration"
   depends_on "cairo"
   depends_on "pygtk"
-
   depends_on "glib"
   depends_on "gdk-pixbuf"
   depends_on "freetype"
-  depends_on "xz"
-  depends_on "lbzip2"
-  depends_on "aalib"
-  depends_on "ghostscript"
-  depends_on "libpng"
-  depends_on "librsvg"
-  depends_on "libwmf"
-  depends_on "libexif"
+  depends_on "xz" # For LZMA
   depends_on "d-bus"
+  depends_on "aalib"
+  depends_on "librsvg"
+  depends_on "libpng" => :recommended
+  depends_on "libwmf" => :recommended
+  depends_on "libtiff" => :recommended
+  depends_on "ghostscript" => :optional
+  depends_on "poppler" => :optional
+  depends_on "libexif" => :optional
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-glibtest"
+    args = %W[
+      --prefix=#{prefix}
+      --disable-silent-rules
+      --disable-dependency-tracking
+      --disable-glibtest
+      --disable-gtktest
+      --datarootdir=#{share}
+      --sysconfdir=#{etc}
+    ]
+
+    args << "--without-x" if build.without? "x11"
+    args << "--without-libtiff" if build.without? "libtiff"
+    args << "--without-libpng" if build.without? "libpng"
+    args << "--without-wmf" if build.without? "libwmf"
+    args << "--without-poppler" if build.without? "poppler"
+    args << "--without-libexif" if build.without? "libexif"
+
+    system "./configure", *args
     system "make", "install"
   end
 
