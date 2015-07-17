1ec56b00cf6db0f8750cc939737eccf4d777fea9
Misty De Meo <mistydemeo@gmail.com>
2015-07-16 19:25:15 -0700

    geomview: use openmotif

--- geomview.rb
+++ geomview.rb
@@ -2,11 +2,14 @@ require "formula"
 
 class Geomview < Formula
   homepage "http://www.geomview.org"
-  url "https://downloads.sourceforge.net/project/geomview/geomview/1.9.5/geomview-1.9.5.tar.gz"
+  # Restore the SF URL to the default when SF is back again
+  url "http://http.debian.net/debian/pool/main/g/geomview/geomview_1.9.5.orig.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/geomview/geomview/1.9.5/geomview-1.9.5.tar.gz"
   sha1 "26186046dc18ab3872e7104745ae474908ee54d1"
+  revision 1
 
   depends_on :x11
-  depends_on "lesstif"
+  depends_on "openmotif"
 
   def install
     system "./configure", "--disable-dependency-tracking",
