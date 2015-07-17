91c303aa4c6953ce190c6af91f3bcec0319e0a45
Misty De Meo <mistydemeo@gmail.com>
2015-07-16 19:08:45 -0700

    grace: use openmotif

--- grace.rb
+++ grace.rb
@@ -2,6 +2,7 @@ class Grace < Formula
   homepage "http://plasma-gate.weizmann.ac.il/Grace/"
   url "https://mirrors.kernel.org/debian/pool/main/g/grace/grace_5.1.25.orig.tar.gz"
   sha256 "751ab9917ed0f6232073c193aba74046037e185d73b77bab0f5af3e3ff1da2ac"
+  revision 1
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-x11"
@@ -14,7 +15,7 @@ class Grace < Formula
   depends_on "pdflib-lite"
   depends_on "jpeg"
   depends_on "fftw"
-  depends_on "lesstif"
+  depends_on "openmotif"
 
   def install
     ENV.O1 # https://github.com/Homebrew/homebrew/issues/27840#issuecomment-38536704
