b8f8534ff48d2ea7353083ee34ce398b529a1866
Dominyk Tiller <dominyktiller@gmail.com>
2015-07-18 11:08:08 +0100

    geeqie: bump revision for linkage break
    Closes #105
    
    Closes #107.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- geeqie.rb
+++ geeqie.rb
@@ -3,7 +3,7 @@ class Geeqie < Formula
   homepage "http://geeqie.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/geeqie/geeqie/geeqie-1.1/geeqie-1.1.tar.gz"
   sha256 "5544e81c29917a647f19bfe800d9f0dd1cd5b890329feebd9abd80927e1afecf"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-x11"
