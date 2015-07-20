2c892fcef6e84cea8bcdf061d6411ba31e4b162f
Misty De Meo <mistydemeo@gmail.com>
2015-07-20 12:45:04 -0700

    imake: embed GCC's opt_prefix, not prefix
    Fixes #110.
    Closes #111.
    
    Signed-off-by: Misty De Meo <mistydemeo@gmail.com>

--- imake.rb
+++ imake.rb
@@ -1,7 +1,8 @@
 class Imake < Formula
   homepage "http://xorg.freedesktop.org"
   url "http://xorg.freedesktop.org/releases/individual/util/imake-1.0.7.tar.bz2"
-  sha1 "52e236776133f217d438622034b8603d201a6ec5"
+  sha256 "690c2c4ac1fad2470a5ea73156cf930b8040dc821a0da4e322014a42c045f37e"
+  revision 1
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles-x11"
@@ -16,19 +17,19 @@ class Imake < Formula
 
   patch :p0 do
     url "https://trac.macports.org/browser/trunk/dports/x11/imake/files/patch-imakemdep.h.diff?rev=127961&format=txt"
-    sha1 "88a78651635d41072febe735dc76e75d44cf0957"
+    sha256 "1f7a24f625d2611c31540d4304a45f228767becafa37af01e1695d74e612459e"
   end
 
   resource "xorg-cf-files" do
     url "http://xorg.freedesktop.org/releases/individual/util/xorg-cf-files-1.0.5.tar.bz2"
-    sha1 "ae22eb81d56d018f0b3b149f70965ebfef2385fd"
+    sha256 "ed23b85043edecc38fad4229e0ebdb7ff80b570e746bc03a7c8678d601be7ed4"
   end
 
   def install
     ENV.deparallelize
 
     # imake runtime is broken when used with clang's cpp
-    cpp_program = Formula["gcc"].prefix/"bin/cpp-#{Formula["gcc"].version_suffix}"
+    cpp_program = Formula["gcc"].opt_bin/"cpp-#{Formula["gcc"].version_suffix}"
     inreplace "imakemdep.h", /::CPPCMD::/, cpp_program
     inreplace "imake.man", /__cpp__/, cpp_program
 
