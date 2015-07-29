b17d072e9a34ac603f364df9f843ffc13b2ae582
BrewTestBot <brew-test-bot@googlegroups.com>
2015-07-29 13:27:57 +0100

    gnumeric: update 1.12.23 bottle.

--- gnumeric.rb
+++ gnumeric.rb
@@ -5,9 +5,9 @@ class Gnumeric < Formula
   sha256 "00474cbf1e70f62062974d0beb5f65ebc45d1047bc8fd0a1133e3d9725e19551"
 
   bottle do
-    sha256 "29f856544019bc46cbea64a12a94787c664ad5edae716d6ce5914258c2ef2eab" => :yosemite
-    sha256 "d27645b4aaf3066bb593b31aaf00468adaff35244e8fbee1050671b41756e14b" => :mavericks
-    sha256 "d4e2a984bc16c17e6d41bcb1992d16c9fb097bcac40b5ed3a053cf4b7eb4d334" => :mountain_lion
+    sha256 "113d9cb86597b49ac68ee5701fff6b01abc33ebd794767863fb53d9693e29a84" => :yosemite
+    sha256 "87cba0d9ce23e7d884992423349beafaa6b7419beb407cd35c27df0a3b735ea4" => :mavericks
+    sha256 "ca98c7ec558aea34c0a5d618199ab0eb4e878b8c19c6516bc040e0e0c00c3399" => :mountain_lion
   end
 
   option "with-python-scripting", "Enable Python scripting."
