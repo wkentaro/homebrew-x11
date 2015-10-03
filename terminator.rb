9f0fcfa4f22e19d9267dde02ef2a0c60e15a0337
Andrew Janke <andrew@apjanke.net>
2015-10-03 19:05:58 -0400

    terminator: 0.98, add HEAD
    Closes #134.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- terminator.rb
+++ terminator.rb
@@ -1,12 +1,19 @@
 class Terminator < Formula
   desc "Multiple terminals in one window"
-  homepage "http://gnometerminator.blogspot.co.uk/p/introduction.html"
-  url "https://launchpad.net/terminator/trunk/0.97/+download/terminator-0.97.tar.gz"
-  sha256 "9131847023fa22f11cf812f6ceff51b5d66d140b6518ad41d7fa8b0742bfd3f7"
+  homepage "http://gnometerminator.blogspot.com/p/introduction.html"
+  url "https://launchpad.net/terminator/trunk/0.98/+download/terminator-0.98.tar.gz"
+  sha256 "0a6d8c9ffe36d67e60968fbf2752c521e5d498ceda42ef171ad3e966c02f26c1"
+  head "lp:terminator", :using => :bzr
+
+  stable do
+    # Patch to fix cwd resolve issue for OS X / Darwin
+    # See: https://bugs.launchpad.net/terminator/+bug/1261293
+    # Should be fixed in next release after 0.98
+    patch :DATA
+  end
 
   bottle do
     cellar :any
-    revision 1
     sha256 "db25b36de9844473fddd6afa620fc5a4719fd5908e1c6afa74a6b792f6b88985" => :yosemite
     sha256 "16fae780f572189db5b84d86a89c22260e5aa456e7a1941ebb8331aa2bfb1c33" => :mavericks
     sha256 "b0c6d46bc9088520cd271b0e35aad958a6ef4c792fa404a9fbf52d3e3d52de6e" => :mountain_lion
@@ -19,10 +26,6 @@ class Terminator < Formula
   depends_on "pygtk"
   depends_on "pygobject"
   depends_on "pango"
-
-  # Patch to fix cwd resolve issue for OS X / Darwin
-  # See: https://bugs.launchpad.net/terminator/+bug/1261293
-  patch :DATA
 
   def install
     ENV.prepend_create_path "PYTHONPATH", lib/"python2.7/site-packages"
