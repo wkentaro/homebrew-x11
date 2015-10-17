b0863f7d8d8c2d5c5a8f037886b5d3c372f2db83
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2015-10-17 18:02:22 +0100

    zenity 3.18.1
    Closes #144.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- zenity.rb
+++ zenity.rb
@@ -1,8 +1,8 @@
 class Zenity < Formula
   desc "GTK+ dialog boxes for the command-line"
   homepage "https://live.gnome.org/Zenity"
-  url "https://download.gnome.org/sources/zenity/3.18/zenity-3.18.0.tar.xz"
-  sha256 "0efafea95a830f3bf6eca805ff4a8008df760a6ad3e81181b9473dcf721c3a69"
+  url "https://download.gnome.org/sources/zenity/3.18/zenity-3.18.1.tar.xz"
+  sha256 "089d45f8e82bb48ae80fcb78693bcd7a29579631234709d752afed6c5a107ba8"
 
   bottle do
     sha256 "afc146f738d95f38388a1a92eaefe9131d9b3f037d99022f7c16f69e8da440ea" => :el_capitan
@@ -18,9 +18,11 @@ class Zenity < Formula
   depends_on "gnome-doc-utils"
   depends_on "scrollkeeper"
 
+  # submitted upstream at https://bugzilla.gnome.org/show_bug.cgi?id=756756
+  patch :DATA
+
   def install
     ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].opt_lib}/python2.7/site-packages"
-    system "./autogen.sh" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
@@ -30,3 +32,21 @@ class Zenity < Formula
     system "#{bin}/zenity", "--help"
   end
 end
+
+__END__
+diff --git a/src/option.c b/src/option.c
+index 79a6f92..246cf22 100644
+--- a/src/option.c
++++ b/src/option.c
+@@ -2074,9 +2074,11 @@ zenity_text_post_callback (GOptionContext *context,
+     if (zenity_text_font)
+       zenity_option_error (zenity_option_get_name (text_options, &zenity_text_font),
+                            ERROR_SUPPORT);
++#ifdef HAVE_WEBKITGTK
+     if (zenity_text_enable_html)
+       zenity_option_error (zenity_option_get_name (text_options, &zenity_text_enable_html),
+                            ERROR_SUPPORT);
++#endif
+   }
+   return TRUE;
+ }
